4a28fd4b7694201038b3bb03defebc5e504841f4
Minerva2 <3482752714@qq.com>
2017-11-22 13:28:12 +0800

    express项目和路由

--- helloworld/hello.js
+++ helloworld/hello.js
@@ -3,8 +3,12 @@ var express = require('express');
 var hello = express();
 
 hello.get('/hello/',function(req,res){
-	res.send('hello world');
+	res.send('hello');
 	});
+	
+hello.get('/hello.world',function(req,res){
+	res.send('hello world');
+	});	
 	
 hello.listen(8080,function(){
 	console.log('port 8080 is running');
